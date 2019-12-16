b06eee74cf3c791408170bba9dab317d207dab2d
RomanMunar <RomanMunar.1@gmail.com>
2019-12-16 08:10:21 +0800

    Updates

---
+++ precache-manifest.78865b6984b5d62f729c454c7698490b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "247b791274e29de2e0deeb8693dedeae",
+    "url": "/reactBasicMvcBoiler/index.html"
+  },
+  {
+    "revision": "e2677b38b759409d9239",
+    "url": "/reactBasicMvcBoiler/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "9c8f9c7921d84d580599",
+    "url": "/reactBasicMvcBoiler/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "e2677b38b759409d9239",
+    "url": "/reactBasicMvcBoiler/static/js/2.5d9824f7.chunk.js"
+  },
+  {
+    "revision": "d7c02b5cfd2fce7b07312cd42a7244b2",
+    "url": "/reactBasicMvcBoiler/static/js/2.5d9824f7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9c8f9c7921d84d580599",
+    "url": "/reactBasicMvcBoiler/static/js/main.6702cc41.chunk.js"
+  },
+  {
+    "revision": "a1cdcbbe60e83bdf0010",
+    "url": "/reactBasicMvcBoiler/static/js/runtime-main.b79d39ae.js"
+  }
+]);
